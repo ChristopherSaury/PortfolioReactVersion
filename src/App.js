6d4d449e32760daf87117a8b763be3386f815869
ChristopherSaury <saurychristopher@gmail.com>
2021-03-10 10:48:12 +0100

    A route has been created with react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
-import HomePage from './components/HomePage/HomePage';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './components/Page/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+      </Switch>
     </div>
   );
 }
