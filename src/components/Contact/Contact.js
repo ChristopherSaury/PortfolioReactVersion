9e94512d36342d576f8f5b6c35ddc04f904a3f49
ChristopherSaury <saurychristopher@gmail.com>
2021-03-10 11:00:03 +0100

    The Contact component has been created. It can send data on the back server with axios

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,75 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Contact.css';
 
 export default class Contact extends Component {
+    state={
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+        sent: false
+    }
+
+    handleName = (e) => {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    handleEmail = (e) => {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    handleSubject = (e) => {
+        this.setState({
+            subject: e.target.value
+        });
+    }
+
+    handleMessage = (e) => {
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    formSubmit = (e) => {
+        e.preventDefault();
+
+        const data = {
+            name: this.state.name,
+            email: this.state.email,
+            subject: this.state.subject,
+            message: this.state.message
+        }
+
+        axios.post('/contact', data)
+        .then(res => {
+            this.setState({
+                sent: true
+            }, this.resetForm());
+        })
+        .catch(()=>{
+           console.log('message not send') 
+        })
+    }
+
+    resetForm = () => {
+        this.setState({
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        });
+        setTimeout(() => {
+            this.setState({
+              sent: false
+            });
+          }, 3000);
+    }
+
     render() {
         return (
             <section className='contact' id='contact'>
@@ -47,20 +115,45 @@ export default class Contact extends Component {
                     </div>
                     <div className='column right'>
                         <div class="text">Envoyer un message</div>
-                        <form>
+                        <form onSubmit={this.formSubmit}>
                             <div className='fields'>
                                 <div className='field name'>
-                                    <input type='text' placeholder='Votre nom' required/>
+                                    <input
+                                    type='text'
+                                    name='name'
+                                    placeholder='Votre nom'
+                                    value={this.state.name}
+                                    onChange={this.handleName}
+                                    required/>
                                 </div>
                                 <div className='field email'>
-                                    <input type='email' placeholder='Votre email' required/>
+                                    <input
+                                    type='email'
+                                    name='email'
+                                    placeholder='Votre email'
+                                    value={this.state.email}
+                                    onChange={this.handleEmail}
+                                    required/>
                                 </div>
                             </div>
                                 <div className='field subject'>
-                                    <input type='text' placeholder='Sujet du message' required/>
+                                    <input
+                                    type='text'
+                                    name='subject'
+                                    placeholder='Sujet du message'
+                                    value={this.state.subject}
+                                    onChange={this.handleSubject}
+                                    required/>
                                 </div>
                                 <div className='field textarea'>
-                                <textarea cols="30" rows="10" placeholder="Message..." required></textarea>
+                                    <textarea
+                                    name='message'
+                                    cols="30"
+                                    rows="10"
+                                    placeholder="Message..."
+                                    value={this.state.message}
+                                    onChange={this.handleMessage}
+                                    required></textarea>
                                 </div>
                                 <div className='button'>
                                     <button type='submit'>Envoyer</button>
