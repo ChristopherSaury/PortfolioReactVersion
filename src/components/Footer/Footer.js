66fd21e417f2c970ef0e0c7febcd104ea7b8f0df
ChristopherSaury <saurychristopher@gmail.com>
2021-03-10 10:57:41 +0100

    The Footer component has been created

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+        <span>Portfolio de Christopher Saury | <span class='far fa-copyright'></span> {new Date().getUTCFullYear()} Tous droits réservés.</span>
+    </footer>
+    )
+}
+
+export default Footer
